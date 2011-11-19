8d81087291f19d0fea66d396e1199590b8fec9ec
Gary <cooljinx@gmail.com>
2011-11-19 18:13:24 +0530

    Done with user edit/update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,12 +14,26 @@ module SessionsHelper
   end
   
   def signed_in?
-    !current_user.nil?
+    !current_user.nil?     # returns true if user is signed in
   end
   
   def sign_out
     cookies.delete(:remember_token)
     current_user = nil
+  end
+  
+  def deny_access
+    store_location          # for friendly forwarding
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+  
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
   end
   
   private
@@ -30,6 +44,14 @@ module SessionsHelper
       
       def remember_token
           cookies.signed[:remember_token] || [nil, nil]   # returns nil if cookies.signed[:remember_token] is itself nil
+      end
+      
+      def store_location
+        session[:return_to] = request.fullpath
+      end
+
+      def clear_return_to
+        session[:return_to] = nil
       end
         
 end
---
+++ lib/tasks/sample_data.rake
@@ -0,0 +1,23 @@
+require 'faker'
+
+namespace :db do
+  desc "Fill database with sample data"
+  task :populate => :environment do
+    Rake::Task['db:reset'].invoke
+    admin = User.create!(:name => "Example User",
+                         :email => "example@railstutorial.org",
+                         :password => "foobar",
+                         :password_confirmation => "foobar")
+    admin.toggle!(:admin)         # default is 'false'. toggle that to make first sample user Admin
+
+    99.times do |n|
+      name = Faker::Name.name
+      email = "example-#{n+1}@railstutorial.org"
+      password = "password"
+      User.create!(:name => name,
+                          :email => email,
+                          :password => password,
+                          :password_confirmation => password)
+    end
+  end
+end
